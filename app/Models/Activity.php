c1c09e003588da5a2ab746d25e2d8e91662a2954
siddartha <siddartha7121@gmail.com>
2025-12-08 16:09:51 +0530

    jsomn

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -3,17 +3,32 @@
 namespace App\Models;
 
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Storage;
 
 class Activity
 {
     public static function fetchActivities()
     {
-        $response = Http::get("https://kp-lms-static.s3.us-east-2.amazonaws.com/activities.json");
+        $path = 'activities.json';
 
-        if ($response->failed()) {
-            return null;
+        // 1️⃣ Check if local JSON exists
+        if (Storage::exists($path)) {
+            $json = Storage::get($path);
+            return json_decode($json, true);
         }
 
-        return $response->json();
+        // 2️⃣ If NOT exist → Call API
+        $response = Http::withoutVerifying()->get("https://kp-lms-static.s3.us-east-2.amazonaws.com/activities.json");
+
+        if ($response->failed()) {
+            return null;   // API also failed
+        }
+
+        $data = $response->json();
+
+        // 3️⃣ Save API data locally for next time
+        Storage::put($path, json_encode($data, JSON_PRETTY_PRINT));
+
+        return $data;
     }
 }
